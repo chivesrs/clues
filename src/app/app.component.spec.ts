9255f7ee45e6ef83994150e5359e4e93f61405b5
Chives <chivesrs@gmail.com>
2019-07-16 14:22:44 -0400

    Results of running npm update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled: HTMLDivElement = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Scan Clue Guide!');
+    expect(compiled.querySelector('h1')!.textContent).toContain('Welcome to Scan Clue Guide!');
   });
 });
